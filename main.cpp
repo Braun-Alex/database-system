e5bee134f662d0756aec08ad962d98e1dc1dcb45
Braun-Alex <lamanasakasa@gmail.com>
2023-09-25 04:47:13 +0300

    HTTP request handlers have been added

--- main.cpp
+++ main.cpp
@@ -15,13 +15,298 @@ class DatabaseCreateRequestHandler: public HTTPRequestHandler
     void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response) override
     {
         Application& app = Application::instance();
-        app.logger().information("Request from %s", request.clientAddress().toString());
+        app.logger().information("Request \"Create database\"  from %s",
+                                 request.clientAddress().toString());
 
         response.setChunkedTransferEncoding(true);
         response.setContentType("text/html");
 
         auto databaseName = request.find("databaseName");
         if (databaseName == request.end()) {
+            response.setStatus(HTTPResponse::HTTP_BAD_REQUEST);
+        } else {
+            response.setStatus(HTTPResponse::HTTP_OK);
+        }
+        response.send();
+    }
+};
+
+class DatabaseShowRequestHandler: public HTTPRequestHandler
+{
+    void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response) override
+    {
+        Application& app = Application::instance();
+        app.logger().information("Request \"Show database\" from %s",
+                                 request.clientAddress().toString());
+
+        response.setChunkedTransferEncoding(true);
+        response.setContentType("text/html");
+
+        auto databaseName = request.find("databaseName");
+        if (databaseName == request.end()) {
+            response.setStatus(HTTPResponse::HTTP_BAD_REQUEST);
+        } else {
+            response.setStatus(HTTPResponse::HTTP_OK);
+        }
+        response.send();
+    }
+};
+
+class DatabaseRenameRequestHandler: public HTTPRequestHandler
+{
+    void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response) override
+    {
+        Application& app = Application::instance();
+        app.logger().information("Request \"Rename database\" from %s",
+                                 request.clientAddress().toString());
+
+        response.setChunkedTransferEncoding(true);
+        response.setContentType("text/html");
+
+        auto databaseName = request.find("databaseName"),
+        databaseNewName = request.find("databaseNewName");
+        if (databaseName == request.end() && databaseNewName == request.end()) {
+            response.setStatus(HTTPResponse::HTTP_BAD_REQUEST);
+        } else {
+            response.setStatus(HTTPResponse::HTTP_OK);
+        }
+        response.send();
+    }
+};
+
+class DatabaseDeleteRequestHandler: public HTTPRequestHandler
+{
+    void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response) override
+    {
+        Application& app = Application::instance();
+        app.logger().information("Request \"Delete database\" from %s",
+                                 request.clientAddress().toString());
+
+        response.setChunkedTransferEncoding(true);
+        response.setContentType("text/html");
+
+        auto databaseName = request.find("databaseName");
+        if (databaseName == request.end()) {
+            response.setStatus(HTTPResponse::HTTP_BAD_REQUEST);
+        } else {
+            response.setStatus(HTTPResponse::HTTP_OK);
+        }
+        response.send();
+    }
+};
+
+class TableCreateRequestHandler: public HTTPRequestHandler
+{
+    void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response) override
+    {
+        Application& app = Application::instance();
+        app.logger().information("Request \"Create table\" from %s",
+                                 request.clientAddress().toString());
+
+        response.setChunkedTransferEncoding(true);
+        response.setContentType("text/html");
+
+        auto databaseName = request.find("databaseName"),
+                tableName = request.find("tableName");
+        if (databaseName == request.end() && tableName == request.end()) {
+            response.setStatus(HTTPResponse::HTTP_BAD_REQUEST);
+        } else {
+            response.setStatus(HTTPResponse::HTTP_OK);
+        }
+        response.send();
+    }
+};
+
+class TableShowRequestHandler: public HTTPRequestHandler
+{
+    void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response) override
+    {
+        Application& app = Application::instance();
+        app.logger().information("Request \"Show table\" from %s",
+                                 request.clientAddress().toString());
+
+        response.setChunkedTransferEncoding(true);
+        response.setContentType("text/html");
+
+        auto databaseName = request.find("databaseName"),
+                tableName = request.find("tableName");
+        if (databaseName == request.end() && tableName == request.end()) {
+            response.setStatus(HTTPResponse::HTTP_BAD_REQUEST);
+        } else {
+            response.setStatus(HTTPResponse::HTTP_OK);
+        }
+        response.send();
+    }
+};
+
+class TableRenameRequestHandler: public HTTPRequestHandler
+{
+    void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response) override
+    {
+        Application& app = Application::instance();
+        app.logger().information("Request \"Rename table\" from %s",
+                                 request.clientAddress().toString());
+
+        response.setChunkedTransferEncoding(true);
+        response.setContentType("text/html");
+
+        auto databaseName = request.find("databaseName"),
+                tableName = request.find("tableName");
+        if (databaseName == request.end() && tableName == request.end()) {
+            response.setStatus(HTTPResponse::HTTP_BAD_REQUEST);
+        } else {
+            response.setStatus(HTTPResponse::HTTP_OK);
+        }
+        response.send();
+    }
+};
+
+class TableDeleteRequestHandler: public HTTPRequestHandler
+{
+    void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response) override
+    {
+        Application& app = Application::instance();
+        app.logger().information("Request \"Delete table\" from %s",
+                                 request.clientAddress().toString());
+
+        response.setChunkedTransferEncoding(true);
+        response.setContentType("text/html");
+
+        auto databaseName = request.find("databaseName"),
+                tableName = request.find("tableName");
+        if (databaseName == request.end() && tableName == request.end()) {
+            response.setStatus(HTTPResponse::HTTP_BAD_REQUEST);
+        } else {
+            response.setStatus(HTTPResponse::HTTP_OK);
+        }
+        response.send();
+    }
+};
+
+class RowAddRequestHandler: public HTTPRequestHandler
+{
+    void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response) override
+    {
+        Application& app = Application::instance();
+        app.logger().information("Request \"Add row\" from %s",
+                                 request.clientAddress().toString());
+
+        response.setChunkedTransferEncoding(true);
+        response.setContentType("text/html");
+
+        auto databaseName = request.find("databaseName"),
+                tableName = request.find("tableName");
+        if (databaseName == request.end() && tableName == request.end()) {
+            response.setStatus(HTTPResponse::HTTP_BAD_REQUEST);
+        } else {
+            response.setStatus(HTTPResponse::HTTP_OK);
+        }
+        response.send();
+    }
+};
+
+class RowEditRequestHandler: public HTTPRequestHandler
+{
+    void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response) override
+    {
+        Application& app = Application::instance();
+        app.logger().information("Request \"Edit row\" from %s",
+                                 request.clientAddress().toString());
+
+        response.setChunkedTransferEncoding(true);
+        response.setContentType("text/html");
+
+        auto databaseName = request.find("databaseName"),
+                tableName = request.find("tableName");
+        if (databaseName == request.end() && tableName == request.end()) {
+            response.setStatus(HTTPResponse::HTTP_BAD_REQUEST);
+        } else {
+            response.setStatus(HTTPResponse::HTTP_OK);
+        }
+        response.send();
+    }
+};
+
+class RowDeleteRequestHandler: public HTTPRequestHandler
+{
+    void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response) override
+    {
+        Application& app = Application::instance();
+        app.logger().information("Request \"Delete row\" from %s",
+                                 request.clientAddress().toString());
+
+        response.setChunkedTransferEncoding(true);
+        response.setContentType("text/html");
+
+        auto databaseName = request.find("databaseName"),
+                tableName = request.find("tableName");
+        if (databaseName == request.end() && tableName == request.end()) {
+            response.setStatus(HTTPResponse::HTTP_BAD_REQUEST);
+        } else {
+            response.setStatus(HTTPResponse::HTTP_OK);
+        }
+        response.send();
+    }
+};
+
+class AttributeAddRequestHandler: public HTTPRequestHandler
+{
+    void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response) override
+    {
+        Application& app = Application::instance();
+        app.logger().information("Request \"Add attribute\" from %s",
+                                 request.clientAddress().toString());
+
+        response.setChunkedTransferEncoding(true);
+        response.setContentType("text/html");
+
+        auto databaseName = request.find("databaseName"),
+                tableName = request.find("tableName");
+        if (databaseName == request.end() && tableName == request.end()) {
+            response.setStatus(HTTPResponse::HTTP_BAD_REQUEST);
+        } else {
+            response.setStatus(HTTPResponse::HTTP_OK);
+        }
+        response.send();
+    }
+};
+
+class AttributeEditRequestHandler: public HTTPRequestHandler
+{
+    void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response) override
+    {
+        Application& app = Application::instance();
+        app.logger().information("Request \"Edit attribute\" from %s",
+                                 request.clientAddress().toString());
+
+        response.setChunkedTransferEncoding(true);
+        response.setContentType("text/html");
+
+        auto databaseName = request.find("databaseName"),
+                tableName = request.find("tableName");
+        if (databaseName == request.end() && tableName == request.end()) {
+            response.setStatus(HTTPResponse::HTTP_BAD_REQUEST);
+        } else {
+            response.setStatus(HTTPResponse::HTTP_OK);
+        }
+        response.send();
+    }
+};
+
+class AttributeDeleteRequestHandler: public HTTPRequestHandler
+{
+    void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response) override
+    {
+        Application& app = Application::instance();
+        app.logger().information("Request \"Delete attribute\" from %s",
+                                 request.clientAddress().toString());
+
+        response.setChunkedTransferEncoding(true);
+        response.setContentType("text/html");
+
+        auto databaseName = request.find("databaseName"),
+                tableName = request.find("tableName");
+        if (databaseName == request.end() && tableName == request.end()) {
             response.setStatus(HTTPResponse::HTTP_BAD_REQUEST);
         } else {
             response.setStatus(HTTPResponse::HTTP_OK);
@@ -37,6 +322,32 @@ class RequestHandlerFactory: public HTTPRequestHandlerFactory
         const auto& uri = request.getURI();
         if (uri == "/database/create") {
             return new DatabaseCreateRequestHandler();
+        } else if (uri == "/database/show") {
+            return new DatabaseShowRequestHandler();
+        } else if (uri == "/database/rename") {
+            return new DatabaseRenameRequestHandler();
+        } else if (uri == "/database/delete") {
+            return new DatabaseDeleteRequestHandler();
+        } else if (uri == "table/create") {
+            return new TableCreateRequestHandler();
+        } else if (uri == "table/show") {
+            return new TableShowRequestHandler();
+        } else if (uri == "table/rename") {
+            return new TableRenameRequestHandler();
+        } else if (uri == "table/delete") {
+            return new TableDeleteRequestHandler();
+        } else if (uri == "row/add") {
+            return new RowAddRequestHandler();
+        } else if (uri == "row/edit") {
+            return new RowEditRequestHandler();
+        } else if (uri == "row/delete") {
+            return new RowDeleteRequestHandler();
+        } else if (uri == "attribute/add") {
+            return new AttributeAddRequestHandler();
+        } else if (uri == "attribute/edit") {
+            return new AttributeEditRequestHandler();
+        } else if (uri == "attribute/delete") {
+            return new AttributeDeleteRequestHandler();
         }
     }
 };
