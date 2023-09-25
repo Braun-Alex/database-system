09fee0a913cb3719153e203988f6b52a3a4e6cd2
Braun-Alex <lamanasakasa@gmail.com>
2023-09-25 03:45:36 +0300

    HTTP server has been added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,64 @@
+#include "Poco/Net/HTTPServer.h"
+#include "Poco/Net/HTTPRequestHandler.h"
+#include "Poco/Net/HTTPRequestHandlerFactory.h"
+#include "Poco/Net/HTTPServerRequest.h"
+#include "Poco/Net/HTTPServerResponse.h"
+#include "Poco/Util/ServerApplication.h"
 #include <iostream>
 
-int main() {
-    std::cout << "AAA";
-    return 0;
-}
+using namespace Poco;
+using namespace Poco::Net;
+using namespace Poco::Util;
+
+class HelloRequestHandler: public HTTPRequestHandler
+{
+    void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response) override
+    {
+        Application& app = Application::instance();
+        app.logger().information("Request from %s", request.clientAddress().toString());
+
+        response.setChunkedTransferEncoding(true);
+        response.setContentType("text/html");
+
+        response.send()
+                << "<html>"
+                << "<head><title>Hello</title></head>"
+                << "<body><h1>Hi from Alex Braun</h1></body>"
+                << "</html>";
+    }
+};
+
+class RequestHandlerFactory: public HTTPRequestHandlerFactory
+{
+    HTTPRequestHandler* createRequestHandler(const HTTPServerRequest& req) override
+    {
+        const auto& uri = req.getURI();
+    }
+};
+
+class WebServerApp: public ServerApplication
+{
+    void initialize(Application& self) override
+    {
+        loadConfiguration();
+        ServerApplication::initialize(self);
+    }
+
+    int main(const std::vector<std::string>&) override
+    {
+        UInt16 port = static_cast<UInt16>(config().getUInt("port", 8080));
+        auto* params = new HTTPServerParams;
+        params->setMaxQueued(100);
+        params->setMaxThreads(16);
+        HTTPServer srv(new RequestHandlerFactory, port, params);
+        srv.start();
+        logger().information("HTTP Server started on port %hu.", port);
+        waitForTerminationRequest();
+        logger().information("Stopping HTTP Server...");
+        srv.stop();
+
+        return Application::EXIT_OK;
+    }
+};
+
+POCO_SERVER_MAIN(WebServerApp)
