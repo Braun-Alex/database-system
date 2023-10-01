babbb70db3e9dc49dfe07711490166289484f808
Braun-Alex <lamanasakasa@gmail.com>
2023-10-01 12:18:19 +0300

    Database REST API on the client side has been implemented

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,13 +1,28 @@
 <template>
-  <div class="q-pa-md">
+  <div class="q-pa-md" v-show="!tableView">
       <div class="q-pa-md q-gutter-md">
-        <q-btn :loading="!retrievingFinished" square color="blue" glossy @click="retrieveDatabases()" no-caps>
+        <q-btn :loading="!retrievingFinished" square color="blue" glossy @click="showDatabases()" no-caps>
           Переглянути наявні бази даних
         </q-btn>
 
-        <q-btn :loading="!retrievingFinished" square color="green" glossy @click="createDatabase()" no-caps>
-          Створити нову базу даних
-        </q-btn>
+        <q-btn label="Створити нову базу даних" square color="green" glossy @click="newDatabasePrompt = true" no-caps />
+
+        <q-dialog v-model="newDatabasePrompt" persistent>
+          <q-card>
+            <q-card-section>
+              <div class="text-h6">Вкажіть назву нової бази даних</div>
+            </q-card-section>
+
+            <q-card-section class="q-pt-none">
+              <q-input dense v-model="databaseName" autofocus @keyup.enter="newDatabasePrompt = false" />
+            </q-card-section>
+
+            <q-card-actions align="right" class="text-primary">
+              <q-btn flat label="Відмінити" v-close-popup no-caps />
+              <q-btn flat label="Створити базу даних" v-close-popup no-caps @click="createDatabase()" />
+            </q-card-actions>
+          </q-card>
+        </q-dialog>
 
         <transition
           appear
@@ -24,14 +39,35 @@
               <q-item-section top class="col-2 gt-sm">
                 <q-item-label class="q-mt-sm">
                   {{ database }}
-                  <q-tooltip delay="1500">Дата створення</q-tooltip>
                 </q-item-label>
               </q-item-section>
 
               <q-item-section top side>
                 <div class="text-grey-8 q-gutter-xs">
-                  <q-btn round color="teal" icon="ballot">
+                  <q-btn round color="teal" icon="ballot" @click="showTables(database)">
                     <q-tooltip>Переглянути базу даних</q-tooltip>
+                  </q-btn>
+                  <q-btn round color="amber" icon="camera" @click="renamingDatabasePrompt = true">
+                    <q-tooltip>Перейменувати базу даних</q-tooltip>
+                  </q-btn>
+                  <q-dialog v-model="renamingDatabasePrompt" persistent>
+                    <q-card>
+                      <q-card-section>
+                        <div class="text-h6">Вкажіть назву нової бази даних</div>
+                      </q-card-section>
+
+                      <q-card-section class="q-pt-none">
+                        <q-input dense v-model="databaseName" autofocus @keyup.enter="renamingDatabasePrompt = false" />
+                      </q-card-section>
+
+                      <q-card-actions align="right" class="text-primary">
+                        <q-btn flat label="Відмінити" v-close-popup no-caps />
+                        <q-btn flat label="Перейменувати базу даних" v-close-popup no-caps @click="renameDatabase(database)" />
+                      </q-card-actions>
+                    </q-card>
+                  </q-dialog>
+                  <q-btn round color="red" icon="layers_clear" @click="deleteDatabase(database)">
+                    <q-tooltip>Видалити базу даних</q-tooltip>
                   </q-btn>
                 </div>
               </q-item-section>
@@ -47,20 +83,32 @@
         />
       </div>
   </div>
+  <div class="q-pa-md" v-show="tableView">
+    <p>{{ allDatabases }}</p>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref, Ref } from 'vue'
+const retrievingFinished: Ref<boolean> = ref<boolean>(true)
+const newDatabasePrompt: Ref<boolean> = ref<boolean>(false)
+const renamingDatabasePrompt: Ref<boolean> = ref<boolean>(false)
+const tableView: Ref<boolean> = ref<boolean>(false)
+const databaseName: Ref<string> = ref<string>('')
 const allDatabases: Ref<string[]> = ref<string[]>([])
 import axios from 'axios'
-import { date, useQuasar } from 'quasar'
+import { useQuasar } from 'quasar'
 const $q = useQuasar()
-const retrievingFinished: Ref<boolean> = ref<boolean>(true)
 
-function retrieveDatabases () {
+function showDatabases () {
   retrievingFinished.value = false
   axios.get('http://localhost:8080/show').then((response: any) => {
-    allDatabases.value = response.data.allDatabases
+    allDatabases.value = []
+    response.data.Databases.forEach((database: string) => {
+      if (database !== 'template0' && database !== 'template1' && database !== 'postgres') {
+        allDatabases.value.push(database)
+      }
+    })
   }).catch((error) => {
     $q.notify({
       type: 'negative',
@@ -69,5 +117,118 @@ function retrieveDatabases () {
   }).finally(() => {
     retrievingFinished.value = true
   })
+}
+
+function createDatabase () {
+  axios.get('http://localhost:8080/show').then((response: any) => {
+    allDatabases.value = []
+    response.data.Databases.forEach((database: string) => {
+      if (database !== 'template0' && database !== 'template1' && database !== 'postgres') {
+        allDatabases.value.push(database)
+      }
+    })
+  })
+  if (allDatabases.value.includes(databaseName.value)) {
+    $q.notify({
+      type: 'negative',
+      message: 'Така база даних вже існує. Відмова у створенні'
+    })
+  } else {
+    const formData = new FormData()
+    formData.append('databaseName', databaseName.value)
+    axios.post('http://localhost:8080/database/create', formData).then(() => {
+      $q.notify({
+        type: 'positive',
+        message: 'Базу даних було успішно створено'
+      })
+      allDatabases.value.push(databaseName.value)
+    }).catch((error) => {
+      $q.notify({
+        type: 'negative',
+        message: 'Помилка при створенні бази даних. Причина: ' + error.message
+      })
+    }).finally(() => {
+      databaseName.value = ''
+    })
+  }
+}
+
+function showTables (databaseName: string) {
+  axios.get(`http://localhost:8080/database/show/${databaseName}`).then((response: any) => {
+    allDatabases.value = response.data.Tables
+    tableView.value = true
+  }).catch((error) => {
+    $q.notify({
+      type: 'negative',
+      message: 'API сервер не працює. Причина: ' + error.message
+    })
+  })
+}
+
+function renameDatabase (databasePreviousName: string) {
+  axios.get('http://localhost:8080/show').then((response: any) => {
+    allDatabases.value = []
+    response.data.Databases.forEach((database: string) => {
+      if (database !== 'template0' && database !== 'template1' && database !== 'postgres') {
+        allDatabases.value.push(database)
+      }
+    })
+  })
+  if (allDatabases.value.includes(databaseName.value)) {
+    $q.notify({
+      type: 'negative',
+      message: 'Така база даних вже існує. Відмова у перейменуванні'
+    })
+  } else {
+    const formData = new FormData()
+    formData.append('databasePreviousName', databasePreviousName)
+    formData.append('databaseNewName', databaseName.value)
+    axios.post('http://localhost:8080/database/rename', formData).then(() => {
+      $q.notify({
+        type: 'positive',
+        message: 'Базу даних було успішно перейменовано'
+      })
+      showDatabases()
+    }).catch((error) => {
+      $q.notify({
+        type: 'negative',
+        message: 'Помилка при перейменуванні бази даних. Причина: ' + error.message
+      })
+    }).finally(() => {
+      databaseName.value = ''
+    })
+  }
+}
+
+function deleteDatabase (databaseName: string) {
+  axios.get('http://localhost:8080/show').then((response: any) => {
+    allDatabases.value = []
+    response.data.Databases.forEach((database: string) => {
+      if (database !== 'template0' && database !== 'template1' && database !== 'postgres') {
+        allDatabases.value.push(database)
+      }
+    })
+  })
+  if (!allDatabases.value.includes(databaseName)) {
+    $q.notify({
+      type: 'negative',
+      message: 'Такої бази даних вже не існує. Відмова у видаленні'
+    })
+  } else {
+    const formData = new FormData()
+    formData.append('databaseName', databaseName)
+    axios.post('http://localhost:8080/database/delete', formData).then(() => {
+      $q.notify({
+        type: 'positive',
+        message: 'Базу даних було успішно видалено'
+      })
+      showDatabases()
+    }).catch((error) => {
+      $q.notify({
+        type: 'negative',
+        message: 'Помилка при видаленні бази даних. Причина: ' + error.message
+      })
+    })
+  }
 }
 </script>
--- src/pages/VotingPage.vue
+++ src/pages/VotingPage.vue
@@ -103,4 +103,311 @@ interface Poll {
   countOfVoters: number,
   finished: boolean
 }
+
+const $q = useQuasar()
+const contractAddress: string | undefined = process.env.VUE_APP_BALLOT_CONTRACT
+const allPolls: Ref<Poll[]> = ref<Poll[]>([])
+const watchPolls: Ref<Poll[]> = ref<Poll[]>([])
+const retrievingFinished: Ref<boolean> = ref<boolean>(true)
+const walletConnected: Ref<boolean> = inject<Ref<boolean>>('walletConnected') as Ref<boolean>
+const search: Ref<string> = inject<Ref<string>>('search') as Ref<string>
+const pollIdentifier: Ref<string> = inject<Ref<string>>('pollIdentifier') as Ref<string>
+const authorAddress: Ref<string> = inject<Ref<string>>('authorAddress') as Ref<string>
+const searchPressed: Ref<boolean> = inject<Ref<boolean>>('searchPressed') as Ref<boolean>
+
+watch(searchPressed, () => {
+  if (search.value !== '') {
+    searchPoll(search.value, 'combined')
+  } else if (pollIdentifier.value !== '') {
+    searchPoll(pollIdentifier.value, 'id')
+  } else if (authorAddress.value !== '') {
+    searchPoll(authorAddress.value, 'address')
+  } else {
+    allPolls.value = watchPolls.value
+  }
+})
+
+function searchPoll (searchData: string, searchType: string) {
+  retrievingFinished.value = false
+  if (searchType === 'combined') {
+    const searchValue: string = search.value.toLowerCase()
+    allPolls.value = watchPolls.value.filter((poll: Poll) => {
+      return poll.title.toLowerCase().lastIndexOf(searchValue) !== -1 ||
+        poll.question.toLowerCase().lastIndexOf(searchValue) !== -1
+    })
+  } else if (searchType === 'id') {
+    allPolls.value = watchPolls.value.filter((poll: Poll) => {
+      return poll.id.toString(10) === pollIdentifier.value
+    })
+  } else if (searchType === 'address') {
+    allPolls.value = watchPolls.value.filter((poll: Poll) => {
+      return poll.author === authorAddress.value
+    })
+  }
+  retrievingFinished.value = true
+}
+
+function retrievePolls () {
+  retrievingFinished.value = false
+  allPolls.value = []
+  // @ts-expect-error Window.ethers not TS
+  if (typeof window.ethereum !== 'undefined') {
+    // @ts-expect-error Window.ethers not TS
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const contract = new ethers.Contract(
+      contractAddress as string,
+      abi,
+      provider
+    )
+    contract.getAllPolls().then((data: any) => {
+      data.forEach((poll: any) => {
+        allPolls.value.push({
+          id: allPolls.value.length,
+          title: poll.title,
+          question: poll.question,
+          proposals: poll.proposals,
+          author: poll.author,
+          whenCreated: poll.whenCreated * 1000,
+          duration: poll.duration,
+          countOfVoters: poll.countOfVoters,
+          finished: isFinished(poll.whenCreated * 1000, poll.duration)
+        })
+      })
+      watchPolls.value = allPolls.value
+    }).catch((error: any) => {
+      $q.notify({
+        type: 'negative',
+        message: 'Виникла помилка відображення списку голосувань: ' + error.message
+      })
+    }).finally(() => {
+      retrievingFinished.value = true
+    })
+  } else {
+    $q.notify({
+      type: 'negative',
+      message: 'MetaMask не встановлено'
+    })
+  }
+}
+
+function vote (poll: Poll) {
+  if (!walletConnected.value) {
+    $q.notify({
+      type: 'negative',
+      message: 'Вам потрібно під\'єднати гаманець MetaMask для голосування он-чейн'
+    })
+  } else {
+    const pollItems: Item[] = []
+    poll.proposals.forEach((proposal: string) => {
+      pollItems.push({ label: proposal, value: proposal, color: 'teal', disable: false })
+    })
+    $q.dialog({
+      title: '#' + poll.id + ': ' + poll.title,
+      message: poll.question,
+      options: {
+        type: 'radio',
+        model: poll.proposals[0],
+        items: pollItems
+      },
+      ok: {
+        glossy: true,
+        label: 'Проголосувати',
+        color: 'positive',
+        noCaps: true
+      },
+      cancel: {
+        glossy: true,
+        label: 'Закрити',
+        color: 'negative',
+        noCaps: true
+      },
+      persistent: true
+    }).onOk(choice => {
+      $q.dialog({
+        title: 'Підтвердження',
+        message: 'Ви впевнені у своєму виборі?',
+        ok: {
+          glossy: true,
+          label: 'Так',
+          color: 'positive',
+          noCaps: true
+        },
+        cancel: {
+          glossy: true,
+          label: 'Ні',
+          color: 'negative',
+          noCaps: true
+        },
+        persistent: true
+      }).onOk(() => {
+        // @ts-expect-error Window.ethers not TS
+        if (typeof window.ethereum !== 'undefined') {
+          // @ts-expect-error Window.ethers not TS
+          const provider = new ethers.providers.Web3Provider(window.ethereum)
+          const signer = provider.getSigner()
+          const contract = new ethers.Contract(
+            contractAddress as string,
+            abi,
+            signer
+          )
+          $q.loading.show({
+            spinner: QSpinnerGears,
+            message: 'Підписання транзакції...'
+          })
+          try {
+            contract.vote(
+              poll.id, choice).then((transaction: any) => {
+              $q.loading.hide()
+              $q.loading.show({
+                spinner: QSpinnerGears,
+                message: 'Обробка транзакції...'
+              })
+              transaction.wait().then(() => {
+                $q.notify({
+                  type: 'positive',
+                  icon: 'cloud_done',
+                  message: 'Ви успішно проголосували он-чейн'
+                })
+              }).catch((error: any) => {
+                $q.notify({
+                  type: 'negative',
+                  message: 'У процесі обробки транзакції виникла помилка: ' + error.message
+                })
+              }).finally(() => {
+                $q.loading.hide()
+              })
+            }).catch((error: any) => {
+              $q.loading.hide()
+              const reason: string = error.message
+              if (reason.lastIndexOf('Poll has been finished!') !== -1) {
+                $q.notify({
+                  type: 'negative',
+                  message: 'Голосування вже завершено'
+                })
+              } else if (reason.lastIndexOf('Voter has already voted!') !== -1) {
+                $q.notify({
+                  type: 'negative',
+                  message: 'Ви вже проголосували у даному голосуванні'
+                })
+              } else {
+                $q.notify({
+                  type: 'negative',
+                  message: 'Підписання транзакції було відхилено'
+                })
+              }
+            })
+          } catch (error: any) {
+            $q.notify({
+              type: 'negative',
+              message: 'Виникла помилка у створенні голосування: ' + error.message
+            })
+          }
+        } else {
+          $q.notify({
+            type: 'negative',
+            message: 'MetaMask не встановлено'
+          })
+        }
+      }).onCancel(() => {
+        $q.notify({
+          type: 'negative',
+          message: 'Голосування було відхилено'
+        })
+      })
+    })
+  }
+}
+
+function getResults (poll: Poll) {
+  // @ts-expect-error Window.ethers not TS
+  if (typeof window.ethereum !== 'undefined') {
+    // @ts-expect-error Window.ethers not TS
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const contract = new ethers.Contract(
+      contractAddress as string,
+      abi,
+      provider
+    )
+    contract.getResults(poll.id).then((results: number[]) => {
+      const pollItems: Item[] = []
+      poll.proposals.forEach((proposal: string, index: number) => {
+        const choice: string = results[index] % 10 === 1 ? 'голос' : (
+           results[index] % 10 !== 0 && results[index] % 10 <= 5 ? 'голоси' : 'голосів'
+        )
+        pollItems.push(
+          {
+            label: "Варіант " + "\"" + proposal + "\" набрав " + results[index] + " " + choice,
+            value: proposal,
+            color: 'teal',
+            disable: true
+          })
+      })
+      $q.dialog({
+        title: '#' + poll.id + ': ' + poll.title + '. Результати',
+        message: "Запитання було наступним: " + "\"" + poll.question + "\". Голоси розподілилися таким чином:",
+        options: {
+          type: 'toggle',
+          model: poll.proposals[0],
+          items: pollItems
+        },
+        ok: {
+          glossy: true,
+          label: 'Зрозуміло',
+          color: 'info',
+          noCaps: true
+        },
+        cancel: false
+      })
+    }).catch((error: any) => {
+      const reason: string = error.message
+      if (reason.lastIndexOf('Poll has not been finished yet!') !== -1) {
+        $q.notify({
+          type: 'negative',
+          message: 'Голосування ще триває'
+        })
+      } else {
+        $q.notify({
+          type: 'negative',
+          message: 'Виникла помилка відображення результату голосування: ' + reason
+        })
+      }
+    })
+  } else {
+    $q.notify({
+      type: 'negative',
+      message: 'MetaMask не встановлено'
+    })
+  }
+}
+
+function isFinished (whenCreated: number, duration: number) {
+  const dateOfCreating = new Date(whenCreated)
+  const dateOfClosing = date.addToDate(dateOfCreating, { seconds: duration })
+  const currentDate = Date.now()
+  return currentDate > dateOfClosing.getTime()
+}
+
+function getPollTime (whenCreated: number, duration: number) {
+  if (isFinished(whenCreated, duration)) return 'Завершено'
+  const dateOfCreating = new Date(whenCreated)
+  const dateOfClosing = date.addToDate(dateOfCreating, { seconds: duration })
+  const currentDate = Date.now()
+  let answer = 'Триватиме ще більше '
+  const differenceInHours = date.getDateDiff(dateOfClosing, currentDate, 'hours') - 1
+  if (differenceInHours > 0) {
+    answer += differenceInHours % 10 === 1 ? differenceInHours +
+      ' години' : differenceInHours + ' годин'
+  } else {
+    const differenceInMinutes = date.getDateDiff(dateOfClosing, currentDate, 'minutes') - 1
+    if (differenceInMinutes > 0) {
+      answer += differenceInMinutes % 10 === 1 ? differenceInMinutes +
+        ' хвилини' : differenceInMinutes + ' хвилин'
+    } else {
+      const differenceInSeconds = date.getDateDiff(dateOfClosing, currentDate, 'seconds') - 1
+      answer += differenceInSeconds % 10 === 1 ? differenceInSeconds +
+        ' секунди' : differenceInSeconds + ' секунд'
+    }
+  }
+  return answer
+}
 </script>
